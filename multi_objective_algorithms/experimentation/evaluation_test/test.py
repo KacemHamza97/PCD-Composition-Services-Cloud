19f01e9c4f581a9b3fdfd43c215204989fb34c18
HamzaKAcem97 <hamza.kacem@ensi-uma.tn>
2020-04-10 20:26:59 +0100

    plots

--- multi_objective_algorithms/experimentation/evaluation_test/test.py
+++ multi_objective_algorithms/experimentation/evaluation_test/test.py
@@ -27,7 +27,31 @@ def plot_fx(solutions):
     return f1, f2, f3
 
 
-def plot_algo(true_pareto, solutions, algorithm, hybrid_sol, reference_points=None):
+def plot_5(true_pareto, sol_hybrid, sol_moabc, sol_spea2, sol_nsga2):
+    plt.clf()
+    fig1 = plt.figure()
+    ax = Axes3D(fig1)
+
+    ftp1, ftp2, ftp3 = plot_fx(true_pareto)
+    fh1, fh2, fh3 = plot_fx(sol_hybrid)
+    fm1, fm2, fm3 = plot_fx(sol_moabc)
+    fsp1, fsp2, fsp3 = plot_fx(sol_spea2)
+    fns1, fns2, fns3 = plot_fx(sol_nsga2)
+
+    ax.scatter(ftp1, ftp2, ftp3, marker='^', label='True pareto')
+    ax.scatter(fh1, fh2, fh3, marker='o', label='HYBRID')
+    ax.scatter(fm1, fm2, fm3, marker='s', label='MOABC')
+    ax.scatter(fsp1, fsp2, fsp3, marker='*', label='SPEA2')
+    ax.scatter(fns1, fns2, fns3, marker='+', label='NSGA-II')
+
+    ax.set_xlabel('responseTime')
+    ax.set_ylabel('price')
+    ax.set_zlabel('availability + reliability')
+    ax.legend()
+    plt.savefig(f"plots/plot_all({n_act},{n_candidates},{mcn},{sn},{sq}).png")
+
+
+def plot_3(true_pareto, solutions, algorithm, hybrid_sol, reference_points=None):
     plt.clf()
     ftp1, ftp2, ftp3 = plot_fx(true_pareto)
     f1, f2, f3 = plot_fx(solutions)
@@ -37,7 +61,7 @@ def plot_algo(true_pareto, solutions, algorithm, hybrid_sol, reference_points=No
     ax.scatter(f1, f2, f3, marker='o', label=algorithm)
     if algorithm != 'NSGA-II-R':
         fh1, fh2, fh3 = plot_fx(hybrid_sol)
-        ax.scatter(fh1,fh2,fh3, marker='s', label='Hybrid')
+        ax.scatter(fh1, fh2, fh3, marker='s', label='Hybrid')
     else:
         rf1, rf2, rf3 = plot_fx(reference_points)
         ax.scatter(rf1, rf2, rf3, marker='s', label='reference points')
@@ -47,8 +71,6 @@ def plot_algo(true_pareto, solutions, algorithm, hybrid_sol, reference_points=No
     ax.set_zlabel('availability + reliability')
     ax.legend()
     plt.savefig(f"plots/plot_{algorithm}({n_act},{n_candidates},{mcn},{sn},{sq}).png")
-
-
 
 
 # +----------------------------------------------------------------------------------------------+#
@@ -130,8 +152,8 @@ evaluate(algorithm="SPEA2", solutions=solutions_spea2, pf=true_pareto, rt=rt_spe
 evaluate(algorithm="MOABC", solutions=solutions_moabc, pf=true_pareto, rt=rt_moabc)
 evaluate(algorithm="HYBRID", solutions=solutions_hybrid, pf=true_pareto, rt=rt_hybrid)
 
-
 reference_points = array([[-5, -5, 3], [-10, -10, 1], [-15, -15, 2]])
 
-plot_algo(true_pareto,solutions_moabc,'MOABC',solutions_hybrid)
+# plot_3(true_pareto, solutions_moabc, 'MOABC', solutions_hybrid)
 # plot_algo(true_pareto,solutions_nsga2_r,'NSGA-II-R',solutions_hybrid,reference_points=reference_points)
+plot_5(true_pareto, solutions_hybrid, solutions_moabc, solutions_spea2, solutions_nsga2)
