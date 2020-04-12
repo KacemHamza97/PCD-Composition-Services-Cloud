dfd2c35d9d6febafee2ff571f283736fb128f990
FaroukBouabid <farouk.bouabid@ensi-uma.tn>
2020-04-12 17:37:22 +0100

    simple_test ready

---
+++ multi_objective_algorithms/algorithms/main/hybrid_v0.py
@@ -0,0 +1,88 @@
+from random import uniform, sample
+from data_structure.CompositionPlan import CompositionPlan
+from data_structure.Solution import Solution
+from genetic_operations.implementation import BSG
+from multi_objective_algorithms.algorithms.operations.objective_functions import functions
+from multi_objective_algorithms.algorithms.operations.update import updateSolutions, nonDominatedSort
+
+
+# +----------------------------------------------------------------------------------------------+#
+
+
+# SQ : condition for scouts , MCN : number of iterations , SN : number of ressources , N : n of bees 
+
+def moabc_nsga2_v0(problem, SQ, MCN, SN, N):
+    # solutions  initializing
+    solutionsList = list()
+
+    for i in range(SN):
+        while 1:
+            cp = CompositionPlan(problem.getActGraph(), problem.getCandidates())
+            if cp.verifyConstraints(problem.getConstraints()):
+                solutionsList.append(Solution(cp=cp, fitness=0, functions=functions(cp), probability=0, limit=0))
+                break
+
+    # Algorithm
+    for itera in range(MCN):
+
+        # employed bees phase
+        exploited = sample(solutionsList, N)  # selecting solutions for exploitation randomly
+        U = list()
+        U[:] = solutionsList
+        for sol in exploited:
+            cp1 = sol.cp
+            cp2 = CompositionPlan(problem.getActGraph(), problem.getCandidates())  # randomly generated cp
+            offsprings = BSG(cp1, cp2, problem.getConstraints(), problem.getCandidates())  # BSG
+            # Adding offsprings
+            U += [Solution(cp = cp , fitness = 0 , functions = functions(cp) , probability = 0 , limit = 0) for cp in offsprings]
+        # end of employed bees phase
+
+        fronts = nonDominatedSort(U)
+        solutionsList = updateSolutions(solutionsList, fronts, method="crowdingSort")
+
+        # Probability update
+        s = sum([sol.fitness for sol in solutionsList])
+        for sol in exploited:
+            sol.probability = sol.fitness / s
+
+        # onlooker bees phase
+        probabilityList = [sol.probability for sol in solutionsList]
+        a = min(probabilityList)
+        b = max(probabilityList)
+        U = list()
+        U[:] = solutionsList
+        for sol in solutionsList:
+            if sol.probability > uniform(a, b):
+                cp1 = sol.cp
+                cp2 = CompositionPlan(problem.getActGraph(), problem.getCandidates())  # randomly generated cp
+                offsprings = BSG(cp1, cp2, problem.getConstraints(), problem.getCandidates())  # BSG
+                # Adding offsprings
+                U += [Solution(cp = cp , fitness = 0 , functions = functions(cp) , probability = 0 , limit = 0) for cp in offsprings]
+
+        # end of onlooker bees phase
+
+        fronts = nonDominatedSort(U)
+        solutionsList = updateSolutions(solutionsList, fronts, "crowdingSort")
+        
+
+        # scout bees phase
+        update = 0
+        U = list()
+        U[:] = solutionsList
+        for sol in solutionsList:
+            if sol.limit >= SQ:
+                sol.limit = 0
+                while 1:
+                    cp = CompositionPlan(problem.getActGraph(), problem.getCandidates())  # randomly generated cp
+                    if cp.verifyConstraints(problem.getConstraints()):
+                        U.append(Solution(cp = cp , fitness = 0 , functions = functions(cp) , probability = 0 , limit = 0))
+                        break
+                update = 1
+        # end of scout bees phase
+        if update:
+            fronts = nonDominatedSort(U)
+            solutionsList = updateSolutions(solutionsList, fronts, "crowdingSort")
+
+
+    # end of algorithm
+    return fronts[0]
---
+++ multi_objective_algorithms/experimentation/evaluation_test/simple_test.py
@@ -0,0 +1,175 @@
+import csv
+
+from mpl_toolkits.mplot3d import Axes3D
+from numpy import array
+from time import time
+
+import matplotlib.pyplot as plt
+from data_structure.Problem import Problem
+from multi_objective_algorithms.algorithms.main.hybrid_v0 import moabc_nsga2_v0
+from multi_objective_algorithms.algorithms.main.hybrid import moabc_nsga2
+from multi_objective_algorithms.algorithms.main.nsga2 import nsga2
+from multi_objective_algorithms.algorithms.main.spea2 import spea2
+from multi_objective_algorithms.algorithms.main.nsga2_r import nsga2_r
+from multi_objective_algorithms.algorithms.main.moabc import moabc
+from multi_objective_algorithms.algorithms.operations.update import nonDominatedSort, transform
+from multi_objective_algorithms.experimentation.performance_indicators.performance_indicators import gd, hv, igd
+
+
+
+# +----------------------------------------------------------------------------------------------+#
+def plot_fx(solutions):
+    try:
+        Matrix = transform(solutions)
+    except:
+        Matrix = solutions
+    f1 = Matrix[:, 0]
+    f2 = Matrix[:, 1]
+    f3 = Matrix[:, 2]
+    return f1, f2, f3
+
+
+def plot_3(true_pareto, solutions, algorithm, hybrid_sol, reference_points=None , neighbors = None):
+    plt.clf()
+    fig1 = plt.figure()
+    ax = Axes3D(fig1)
+    ftp1, ftp2, ftp3 = plot_fx(true_pareto)
+    fh1, fh2, fh3 = plot_fx(hybrid_sol)
+    ax.scatter(fh1, fh2, fh3, marker='s', label='Hybrid')
+    ax.scatter(ftp1, ftp2, ftp3, marker='^', label='True pareto')
+    if algorithm != 'NSGA-II-R':
+        f1, f2, f3 = plot_fx(solutions)
+        ax.scatter(f1, f2, f3, marker='o', label=algorithm)
+    else :
+        rf1, rf2, rf3 = plot_fx(reference_points)
+        rn1, rn2, rn3 = plot_fx(neighbors)
+        further_points = []
+        for sol1 in solutions :
+            if sol1.cp not in [sol2.cp for sol2 in neighbors] :
+                further_points.append(sol1)
+        f1, f2, f3 = plot_fx(further_points)
+
+        ax.scatter(rf1, rf2, rf3, marker='s', label='reference points')
+        ax.scatter(rn1, rn2, rn3, marker='d', label='neighboring points')
+        ax.scatter(f1, f2, f3, marker='o', label='rest of solutions')
+
+    ax.set_xlabel('responseTime')
+    ax.set_ylabel('price')
+    ax.set_zlabel('reliability')
+    ax.legend()
+    plt.show()
+
+
+# +----------------------------------------------------------------------------------------------+#
+
+def evaluate(algorithm, pf, **kwargs):
+    HV_list = []
+    GD = 0
+    IGD = 0
+    rt = 0
+    solutions = []
+    for i in range(5):
+        print(f"Executing {algorithm.__name__} ")
+        start_time = time()
+        if algorithm == nsga2_r:
+            solutions, neighbors = algorithm(problem=p, **kwargs)
+        else:
+            solutions = algorithm(problem=p, **kwargs)
+        rt += (time() - start_time) / 5
+        GD += gd(solutions, pf) / 5
+        IGD += igd(solutions, pf) / 5
+        HV = hv(solutions, pf)
+        HV_list.append(HV)
+
+        ##### USED TO GENERATE DATA FOR BOXPLOTS ######################################################
+        #with open('hv_abstract.csv', mode='a') as file:
+        #    file_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        #    file_writer.writerow([algorithm.__name__, n_act, HV])
+        #with open('hv_concrete.csv', mode='a') as file:
+        #    file_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        #    file_writer.writerow([algorithm.__name__, n_candidates, HV])
+        ###############################################################################################
+
+    HV = sum(HV_list) / 5
+    HV_min = min(HV_list)
+    HV_max = max(HV_list)
+    print(f"SCALABILITY = {rt}")
+    print(f"GD = {GD}")
+    print(f"IGD = {IGD}")
+    print(f"HV = {HV}")
+    print(f"HV_max = {HV_max}")
+    print(f"HV_min = {HV_min}")
+    return solutions
+
+# +----------------------------------------------------------------------------------------------+#
+
+# main
+
+# input
+n_act = int(input("NUMBER OF ACTIVITIES : "))
+n_candidates = int(input("NUMBER OF CANDIDATE SERVICES : "))
+constraints = {'responseTime': n_act * 0.3, 'price': n_act * 1.55, 'availability': 0.945 ** n_act, 'reliability': 0.825 ** n_act}
+reference_points = array([[-10, -10, 0.9], [-3, -5, 0.7], [-5, -3, 0.7]])
+mcn = int(input("ITERATION NUMBER / GENERATIONS NUMBER : "))
+sn = int(input("RESSOURCES NUMBER / POPULATION SIZE : "))
+sq = int(input("SCOUT CONDITION : "))
+en = int(input("ARCHIVE SIZE :"))
+x = int(input("1-HYBRID_00\n2-SPEA2\n3-NSGA-II-R\n4-NSGA-II\n5-MOABC\n"))
+
+# problem init
+
+p = Problem(n_act, n_candidates, constraints)
+
+# executing algorithms
+
+paretosList = list()
+print("Finding true pareto ...")
+for itera in range(3):
+    print(f"completed = {itera + 1}/ 3" )
+    if x == 1 :
+        ALGO = "HYBRID_0"
+        print(ALGO)
+        paretosList.extend(moabc_nsga2_v0(problem=p, SQ=sq, MCN=mcn, SN=sn, N=sn // 2))
+    elif x == 2 :
+        ALGO = "SPEA2"
+        print(ALGO)
+        paretosList.extend(spea2(problem=p, G=mcn, N=sn, EN=en))
+    elif x== 3 :
+        ALGO = "NSGA2-R"
+        print(ALGO)
+        paretosList.extend(nsga2_r(problem=p, G=mcn, N=sn, reference_points=reference_points, epsilon=0.2)[0])
+    elif x == 4 :
+        ALGO = "NSGA2"
+        print(ALGO)
+        paretosList.extend(nsga2(problem=p, G=mcn, N=sn))
+    elif x == 5 :
+        ALGO = "MOAB"
+        print(ALGO)
+        paretosList.extend(moabc(problem=p, SQ=sq, MCN=mcn, SN=sn))
+    print(f"HYBRID")
+    paretosList.extend(moabc_nsga2(problem=p, SQ=sq, MCN=mcn, SN=sn, N=sn // 2))
+
+true_pareto = nonDominatedSort(paretosList)[0]
+
+# evaluating algorithms
+print("Evaluating solutions ...")
+if x == 1 :
+    print(ALGO)
+    solutions_algo = evaluate(algorithm=moabc_nsga2_v0, pf=true_pareto, SQ=sq, MCN=mcn, SN=sn, N=sn // 2)
+elif x == 2 :
+    print(ALGO)
+    solutions_algo = evaluate(algorithm=spea2, pf=true_pareto, G=mcn, N=sn, EN=en)
+elif x == 3 :
+    print(ALGO)
+    solutions_algo = evaluate(algorithm=nsga2_r, pf=true_pareto, G=mcn, N=sn, reference_points=reference_points, epsilon=0.2)
+elif x == 4 :
+    print(ALGO)
+    solutions_algo = evaluate(algorithm=nsga2, pf=true_pareto, G=mcn, N=sn)
+elif x == 5 :
+    print(ALGO)
+    solutions_algo = evaluate(algorithm=moabc, pf=true_pareto, SQ=sq, MCN=mcn, SN=sn)
+
+print("HYBRID")
+solutions_hybrid = evaluate(algorithm=moabc_nsga2, pf=true_pareto, SQ=sq, MCN=mcn, SN=sn, N=sn // 2)
+
+plot_3(true_pareto, solutions_algo, ALGO, solutions_hybrid)
